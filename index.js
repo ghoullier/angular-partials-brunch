b95d066d7cd55ab43223f22cec6f30a08b39374d
Grégory Houllier <gregory.houllier@niji.fr>
2014-07-06 01:50:02 +0200

    Implement move template to public directory

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+var fs = require("fs");
+var fspath = require("path");
+var mkdirp = require("mkdirp");
+
 function AngularPartialsPlugin(config) {
   this.config = config;
+  this.publicPath = fspath.resolve(config.paths['public']);
 }
 
 AngularPartialsPlugin.prototype.brunchPlugin = true;
@@ -9,7 +14,10 @@ AngularPartialsPlugin.prototype.type = 'template';
 AngularPartialsPlugin.prototype.extension = 'html';
 
 AngularPartialsPlugin.prototype.compile = function(data, path, callback) {
-  console.log('AngularPartialsPlugin::compile', arguments);
+  var destinationPath = fspath.join(this.publicPath, path.replace('app/', ''));
+  var destinationDir = fspath.dirname(destinationPath);
+  mkdirp.sync(destinationDir);
+  return fs.writeFileSync(destinationPath, data);
 };
 
 module.exports = AngularPartialsPlugin;
